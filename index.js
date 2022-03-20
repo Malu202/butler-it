d7961370443e893a2044202bae1646cea0b16925
Malu202 <poehlmann.martin@gmail.com>
2022-03-20 17:14:51 +0100

    test glitch requests

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
-// Importing http module
 const http = require("http")
+const fetch = require('node-fetch');
 
-// Creating http server
+
 const server = http.createServer((req, res) => {
-
-    // Handling the request
     if (req.url == '/') {
-
-        // Sending the response
-        res.write("<h1>Worked!!<h1>")
-        res.statusCode = 200
-
-        // Ending the response
-        res.end()
+        // makeRequest((response) => {
+        fetch('http://antischwitzomat.glitch.me/currentroommeasurements?user_id=20')
+            .then(res => res.text())
+            .then(body => {
+                res.write("Glitch response:\n" + body);
+                res.statusCode = 200
+                res.end()
+            });
+        // })
     }
 })
 
-// Listening the server
 server.listen(process.env.PORT || 3000, () => {
     console.log("Server is Running")
 })
+
+
+function makeRequest(callback) {
+    const options = {
+        host: "antischwitzomat.glitch.me",
+        path: '/currentroommeasurements?user_id=20',
+        method: 'GET'
+    }
+    http.request(options, (response) => {
+        var str = '';
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
+        response.on('end', function () {
+            callback(str);
+        });
+    }).end();
+
+}
