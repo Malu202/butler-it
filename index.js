eac9300b1120dec5744c79997757e59397a4660a
Malu202 <poehlmann.martin@gmail.com>
2022-03-20 18:18:35 +0100

    test glitch requests

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function forwardRequest(req, res) {
         },
         body: req.body
     });
-    res.status(200).send("sending request");
+    res.status(200).send("sending request: " + req.body);
     // .then(res => res.text())
     // .then(body => {
     //     res.write("Glitch response:\n" + body);
