9e5d19fa1b9cef71346db95044944a5a7dab4ac1
Malu202 <poehlmann.martin@gmail.com>
2022-03-20 16:35:08 +0100

    initial

---
+++ index.js
@@ -0,0 +1,22 @@
+// Importing http module
+const http = require("http")
+
+// Creating http server
+const server = http.createServer((req, res) => {
+
+    // Handling the request
+    if (req.url == '/') {
+
+        // Sending the response
+        res.write("<h1>Worked!!<h1>")
+        res.statusCode = 200
+
+        // Ending the response
+        res.end()
+    }
+})
+
+// Listening the server
+server.listen((3000), () => {
+    console.log("Server is Running")
+})
