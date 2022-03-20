ceac47bc74f25c1b551f6d73c12dd05c7fe45132
Malu202 <poehlmann.martin@gmail.com>
2022-03-20 17:40:09 +0100

    test glitch requests

--- index.js
+++ index.js
@@ -4,15 +4,7 @@ const fetch = require('node-fetch');
 
 const server = http.createServer((req, res) => {
     if (req.url == '/') {
-        // makeRequest((response) => {
-        fetch('http://antischwitzomat.glitch.me/currentroommeasurements?user_id=20')
-            .then(res => res.text())
-            .then(body => {
-                res.write("Glitch response:\n" + body);
-                res.statusCode = 200
-                res.end()
-            });
-        // })
+        forwardRequest(req, res);
     }
 })
 
@@ -20,21 +12,22 @@ server.listen(process.env.PORT || 3000, () => {
     console.log("Server is Running")
 })
 
-
-function makeRequest(callback) {
-    const options = {
-        host: "antischwitzomat.glitch.me",
-        path: '/currentroommeasurements?user_id=20',
-        method: 'GET'
-    }
-    http.request(options, (response) => {
-        var str = '';
-        response.on('data', function (chunk) {
-            str += chunk;
-        });
-        response.on('end', function () {
-            callback(str);
-        });
-    }).end();
-
+async function forwardRequest(req, res) {
+    fetch('http://antischwitzomat.glitch.me/measurements', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: req.body
+    });
+    res.write("sending request");
+    res.statusCode = 200;
+    res.end();
+    // .then(res => res.text())
+    // .then(body => {
+    //     res.write("Glitch response:\n" + body);
+    //     res.statusCode = 200
+    //     res.end()
+    // });
 }
